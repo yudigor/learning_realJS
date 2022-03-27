088c49756678564f1dd50332264ca6f2163d850f
Igor Yudi <40796437+yudigor@users.noreply.github.com>
2022-03-27 18:41:56 -0300

    Learning about arrays

---
+++ scirpt.js
@@ -0,0 +1,7 @@
+// Arrays são conjuntos de dados que podem ser acessados por um indice
+
+let familia = [true, 43, 'Yudelas', 30];
+
+console.log(familia.length);
+console.log(familia);
+console.log(familia[2]);
