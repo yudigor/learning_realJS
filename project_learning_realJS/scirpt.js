399d79049f337a40e29008aafb9636756b967539
Igor Yudi <40796437+yudigor@users.noreply.github.com>
2022-03-27 18:58:53 -0300

    Delete scirpt.js

--- project_learning_realJS/scirpt.js
+++
@@ -1,7 +0,0 @@
-// Arrays são conjuntos de dados que podem ser acessados por um indice
-
-let familia = [true, 43, 'Yudelas', 30];
-
-console.log(familia.length);
-console.log(familia);
-console.log(familia[2]);
