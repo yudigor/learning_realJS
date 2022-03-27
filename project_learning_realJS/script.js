be1feaea4b8c30279db853f36222faacb97d3fa8
Igor Yudi <40796437+yudigor@users.noreply.github.com>
2022-03-27 18:56:05 -0300

    Making functions and assigning parameters

---
+++ project_learning_realJS/script.js
@@ -0,0 +1,13 @@
+// Para nomear uma função: verbo + substantivo
+
+let corSite = "azul";
+
+function resetaCor(cor, tonalidade){
+    corSite = cor + ' ' + tonalidade;
+};
+
+console.log(corSite);
+
+resetaCor("roxo", "escuro");
+
+console.log(corSite);
