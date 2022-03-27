dec6eb8e755b6d4e8bc7fa7584b652e4a7e29507
Igor Yudi <40796437+yudigor@users.noreply.github.com>
2022-03-27 16:00:11 -0300

    Learning about Object creation and its Properties

---
+++ project_learning_realJS/scirpt.js
@@ -0,0 +1,12 @@
+let nome = "Rafael";
+let idade = 25;
+let estaAprovado = true;
+
+let pessoa = {
+    nome: 'Rafael',
+    idade: 25,
+    estaAprovado: true,
+    sobrenome: 'Da Silva'
+};
+
+console.log(pessoa);
